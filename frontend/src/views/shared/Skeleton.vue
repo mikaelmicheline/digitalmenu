d4e93129f82c529de07f746ed0ceb0de1ef3ba85
Mikael Micheline <105074940+mikaelmicheline@users.noreply.github.com>
2024-01-29 11:55:51 -0300

    update Skeleton

--- frontend/src/views/shared/Skeleton.vue
+++ frontend/src/views/shared/Skeleton.vue
@@ -1,42 +1,16 @@
 <template>
   <div>
+    <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, withDefaults, onMounted } from 'vue'
-
-interface Props {
-  width?: string
-  height?: string
-  display?: string
-  backgroundColor?: string
-  borderRadius?: string
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  width: '100%',
-  height: '100%',
-  display: 'inline-block',
-  backgroundColor: 'black',
-  borderRadius: '0'
-})
-
-onMounted(() => {
-  console.log(props)
-})
-
 </script>
 
 <style scoped>
 div {
-  width: v-bind(width);
-  height: v-bind(height);
-  display: v-bind(display);
-  background-color: v-bind(backgroundColor);
   position: relative;
   overflow: hidden;
-  border-radius: v-bind(borderRadius);
 }
 
 div::before {
@@ -45,7 +19,6 @@ div::before {
   width: 40px;
   height: 100%;
   top: 0;
-  left: -40px;
   transform: rotateZ(0deg);
   animation-name: Animation;
   animation-duration: 1.4s;
@@ -57,21 +30,13 @@ div::before {
 }
 
 :root[data-theme="dark"] div::before {
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.1), transparent);
-}
-
-@-webkit-keyframes Animation {
-  0% { left: -40px }
-  100% { left: 100% }
-}
-
-@-moz-keyframes Animation {
-  0% { left: -40px }
-  100% { left: 100% }
+  background: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.06), transparent);
 }
 
 @keyframes Animation {
-  0% { left: -40px }
-  100% { left: 100% }
+  0% { left: -40px; opacity: 0; }
+  40% { opacity: 1; }
+  80% { opacity: 0.5; }
+  100% { left: 100%; opacity: 0; }
 }
 </style>
