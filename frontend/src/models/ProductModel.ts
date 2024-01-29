f4fb0d7c32a0d8f0c4a33487b0d7b7a1ed8de315
Mikael Micheline <105074940+mikaelmicheline@users.noreply.github.com>
2024-01-29 11:34:50 -0300

    add frontend product model

---
+++ frontend/src/models/ProductModel.ts
@@ -0,0 +1,10 @@
+export default interface ProductModel {
+  productId: string
+  companyId: string
+  code: string
+  name: string
+  description: string
+  category: string
+  salePrice: number
+  picture: string
+}
